25b31efba80e46e0075551eadc12e4ee9fc1d69a
kravening <sep_ul_tura@live.nl>
2016-02-23 11:12:53 +0100

    tags naming fix

--- Assets/Scripts/data/Tags.cs
+++ Assets/Scripts/data/Tags.cs
@@ -12,6 +12,6 @@ public static class Tags {
 	public static readonly string ENEMY = "Enemy";
 	public static readonly string STARTINGGRAVITYPOINT = "StartingGravityPoint";
 	public static readonly string CHARGEPAD = "ChargePad";
-    public static readonly string CHARGEPAD = "Planet";
+    public static readonly string PLANET = "Planet";
 
 }

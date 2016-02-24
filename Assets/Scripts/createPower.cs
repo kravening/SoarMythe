71977af3ab866101973d55bba0883058c9d15ae3
delirious-biznasty <nightcookies@gmail.com>
2016-02-24 16:54:23 +0100

    Sun v1
    adding sun to the scene

---
+++ Assets/Scripts/createPower.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class createPower : MonoBehaviour {
+
+	public int getPower;
+
+	// Use this for initialization
+	void Start () {
+		dummyPowerscript.dPower.AddPower (getPower);
+	}
+}
---
+++ Assets/Scripts/objects/BillboardObject.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BillboardObject : MonoBehaviour
+{
+    private Transform myTransform;
+
+    void Awake()
+    {
+        myTransform = gameObject.transform;
+    }
+
+    void Update()
+    {
+        Vector3 v = Camera.main.transform.position - myTransform.position;
+        //v.x = v.z = 0.0f;
+
+        myTransform.LookAt(Camera.main.transform.position);
+    }
+
+}

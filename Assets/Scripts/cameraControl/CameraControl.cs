0ed5733e191ad62d67e46e69595638b4a08e3fbd
Dylan Waij <d_way@live.nl>
2016-03-08 11:34:21 +0100

    audio and check

--- Assets/Scripts/cameraControl/CameraControl.cs
+++ Assets/Scripts/cameraControl/CameraControl.cs
@@ -3,34 +3,32 @@ using System.Collections;
 
 public class CameraControl : MonoBehaviour {
 
-	//private const float Y_ANGLE_MIN = 10.0f;
-	//private const float Y_ANGLE_MAX = 50.0f;
+	const float X_ANGLE_MIN = -50.0f;
+	const float X_ANGLE_MAX = 50.0f;
 
 	public Transform lookAt;
-	private Transform camTransform;
+	Transform camTransform;
 	public float transitionDuration = 2.5f;
-	private Camera cam;
+	Camera cam;
 
 	public float distance = 8.0f;
-	private float currentX = 0.0f;
-	//private float currentY = 0.0f;
-	private float sensitivityX = 4.0f;
-	//private float sensitivityY = 1.0f;
+	float currentX = 0.0f;
+	float sensitivityX = 4.0f;
 
-	private void Start()
+	void Start()
 	{
 		camTransform = transform;
 		cam = Camera.main;
 	}
 
-	private void Update()
+	void Update()
 	{
 		currentX += Input.GetAxis ("Mouse X");
 
-		//currentY = Mathf.Clamp (currentY, Y_ANGLE_MIN, Y_ANGLE_MAX);
+		currentX = Mathf.Clamp (currentX, X_ANGLE_MIN, X_ANGLE_MAX);
 	}
 
-	private void LateUpdate()
+	void LateUpdate()
 	{
 		Vector3 dir = new Vector3 (0, 0, -distance);
 		Quaternion rotation = Quaternion.Euler (0, currentX, 0);

1945667d6f1c7fd41f508bee374d957b792823ea
Dylan Waij <d_way@live.nl>
2016-03-08 11:03:08 +0100

    Cam Control Push

--- Assets/Scripts/cameraControl/CameraControl.cs
+++ Assets/Scripts/cameraControl/CameraControl.cs
@@ -1,62 +1,40 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
 
 public class CameraControl : MonoBehaviour {
 
-	//Vector3 position = Camera.main.transform.position;
+	//private const float Y_ANGLE_MIN = 10.0f;
+	//private const float Y_ANGLE_MAX = 50.0f;
+
 	public Transform lookAt;
-	Transform camTransform;
-	Camera cam;
+	private Transform camTransform;
+	public float transitionDuration = 2.5f;
+	private Camera cam;
 
-	public float CamTargetHeight = 1.0f;
 	public float distance = 8.0f;
-	float currentX = 0.0f;
-	float sensitivityX = 4.0f;
-	Vector3 dir = new Vector3 (-0.26f, 3.44f, -8.37f);
+	private float currentX = 0.0f;
+	//private float currentY = 0.0f;
+	private float sensitivityX = 4.0f;
+	//private float sensitivityY = 1.0f;
 
-	float XboxControlX;
-	float raycastDistance = 15f;
-	float spherecastDistance = 5f;
-	float sphereRadius = 2f;
-	float speedToFixClipping = 0.2f;
-
-	void Start()
+	private void Start()
 	{
-		XboxControlX = GetComponent<Xbox360Wired_InputController> ().RightStickX;
 		camTransform = transform;
 		cam = Camera.main;
 	}
-	void Update()
+
+	private void Update()
 	{
 		currentX += Input.GetAxis ("Mouse X");
-		//currentX += xboxControlX;
+
+		//currentY = Mathf.Clamp (currentY, Y_ANGLE_MIN, Y_ANGLE_MAX);
 	}
-	void LateUpdate()
+
+	private void LateUpdate()
 	{
+		Vector3 dir = new Vector3 (0, 0, -distance);
 		Quaternion rotation = Quaternion.Euler (0, currentX, 0);
 		camTransform.position = lookAt.position + rotation * dir;
 		camTransform.LookAt (lookAt.position);
-	}/*
-	void fixClippingThroughWalls()
-	{
-		RaycastHit hit;
-		Vector3 direction = transform.parent.position - transform.position;
-		Vector3 localPos = transform.localPosition;
-
-		for (float i = offset.z; i <= 0f; i += speedToFixClipping) 
-		{
-			Vector3 pos = transform.TransformPoint (new Vector3 (localPos.x, localPos.y, i));
-			if (Physics.Raycast (pos, direction, out hit, raycastDistance)) 
-			{
-				if (!hit.collider.CompareTag ("Player"))
-					continue;
-				if (!Physics.SphereCast (pos, sphereRadius, transform.forward * -1, out hit, spherecastDistance)) 
-				{
-					localPos.z = i;
-					break;
-				}
-			}
-		}
-		transform.localPosition = Vector3.Lerp (transform.localPosition, localPos, smoothing * Time.deltaTime);
-	}*/
+	}
 }

71f2fbbf9cc3f4ba87c4e59ebd28e7aa27ca289e
Dylan Waij <d_way@live.nl>
2016-03-21 11:04:24 +0100

    Uploading other peoples changes(?)

---
+++ Assets/Scripts/cameraControl/PlayerMovementCameraPosition.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovementCameraPosition : MonoBehaviour {
+
+    [SerializeField][Tooltip("It will follow this Transform to give it's own position to the player.")]
+    Transform camera;
+
+    // Since I'm constantly needing a Quaternion to edit, I'll just have one present in memory that will be reused instead.
+    Quaternion rotation;
+
+    void Start() {
+        rotation = new Quaternion();
+    }
+
+	void Update () {
+        transform.position = camera.position; // Set my position to the camera's.
+        rotation = camera.rotation; // Retrieve the camera's rotation to edit before I apply it.
+        rotation.x = rotation.z = 0; // I only need the Y, so just set X and Z to 0.
+        transform.rotation = rotation; // Then finally change my rotation to what I made.
+	}
+}
---
+++ Assets/Scripts/objects/MovingPlatform.cs
@@ -0,0 +1,73 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class MovingPlatform : MonoBehaviour {
+
+    Transform platform;
+
+    // The points between which the platform loops.
+    [SerializeField][Tooltip("The platform will move between these points, also it will start at the first one in the array.")]
+    List<Transform> points = new List<Transform>();
+    
+    // The container for anything that should be affected by the platform.
+    List<Transform> passengers = new List<Transform>();
+
+    // Create all the integer values I will need. They're edited in the Start(), so their
+    // value has to either be 0 or will not stay that way.
+    int pointsCount, lastPoint, nextPoint = 0;
+
+    // The speed at which the platform moves.
+    [SerializeField][Range(0,0.1f)][Tooltip("How fast the platform will move.")]
+    float speed = 0.5f;
+
+    void Start() {
+        pointsCount = points.Count - 1;
+        nextPoint++;
+        platform = GetComponent<Transform>();
+    }
+
+    void Update() {
+        // This for loop would just grab resources to make two variables it won't use if this is outside the editor.
+        # if UNITY_EDITOR
+        for(int i = 0; i < points.Count; i++) {
+            Transform point = points[i];
+            Transform next = i + 1 >= points.Count ? points[0] : points[i + 1];
+            Debug.DrawLine(point.position, next.position, Color.red, 0, false);
+        }
+        #endif
+
+        if (Vector3.Distance(platform.position, points[nextPoint].position) > 0.5f) {
+            platform.position = Vector3.MoveTowards(platform.position, points[nextPoint].position, speed);
+            for (int i = 0; i < passengers.Count; i++) {
+                Transform passenger = passengers[i];
+                Vector3 customPoint = points[nextPoint].position;
+                customPoint.y = passenger.position.y;
+                passenger.position = Vector3.MoveTowards(passenger.position, customPoint, speed);
+            }
+        } else {
+            if (lastPoint >= pointsCount) {
+                lastPoint = 0;
+            } else {
+                lastPoint++;
+            }
+            if (nextPoint >= pointsCount) {
+                nextPoint = 0;
+            } else {
+                nextPoint++;
+            }
+        }
+    }
+
+    void OnCollisionEnter(Collision other) {
+        Debug.Log(other.collider.name);
+        if (passengers.IndexOf(other.transform) == -1) {
+            passengers.Add(other.transform);
+        }
+    }
+
+    void OnCollisionExit(Collision other) {
+        if (passengers.IndexOf(other.transform) != -1) {
+            passengers.Remove(other.transform);
+        }
+    }
+}

3b5a2c8eacdb049aa9dafecc674d81598a9188d0
Dylan Waij <d_way@live.nl>
2016-03-29 11:56:23 +0200

    Menu Update

---
+++ Assets/Scripts/ui/menuLocator.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.EventSystems;
+
+public class menuLocator : MonoBehaviour
+{
+    public string loadLevel;
+    XboxInputMenu XboxInput;
+    bool aButton;
+    [SerializeField]
+    GameObject[] Buttons;
+
+    void Start()
+    {
+        //xbox360WiredInputController = GetComponent<Xbox360Wired_InputController>();
+    }
+
+    public void pickButton()
+    {
+        if (aButton == true)
+        {/*
+            if ()
+            {
+                
+            }*/
+        }
+    }
+}
